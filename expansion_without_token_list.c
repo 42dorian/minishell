4561062002e50a0f4c8d11976378f27b76dda4d5
Fospany <guthy.koppany@gmail.com>
2026-09-15 10:24:04 +0200

    fixed quoted dollar

--- expansion_without_token_list.c
+++ expansion_without_token_list.c
@@ -6,7 +6,7 @@
 /*   By: bguhty <bguhty@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/13 20:32:29 by guthybarnak       #+#    #+#             */
-/*   Updated: 2026/09/14 16:31:50 by bguhty           ###   ########.fr       */
+/*   Updated: 2026/09/15 10:01:26 by bguhty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,22 @@
 int     within_quotes(int quote_type)
 {
     if (quote_type != 0)
+        return (1);
+    else
+        return (0);
+}
+
+int     is_dollar_or_question_mark(const char letter)
+{
+    if (is_dollar_sign(letter) || is_question_mark(letter))
+        return (1);
+    else
+        return (0);
+}
+
+int     eligible_for_expansion(const char *read_line, int i, int single_quote_counter)
+{
+    if (is_dollar_sign(read_line[i]) && !is_end(read_line[i + 1] && single_quote_counter % 2 == 0) && (is_valid_after_dollar_sign(read_line[i + 1]) || is_dollar_or_question_mark(read_line[i + 1])))
         return (1);
     else
         return (0);
@@ -37,7 +53,7 @@ int     get_full_len_of_expandable_without_token_list(const char *read_line, t_e
     while (read_line[i])
     {
         set_quote_flag_and_count_single_quotes(&quote_flag, &single_quote_counter, read_line[i]);
-        if (is_dollar_sign(read_line[i]) && !is_end(read_line[i + 1] && single_quote_counter % 2 == 0))// && !is_quote(read_line[i + 1]))
+        if (eligible_for_expansion(read_line, i, single_quote_counter))
         {
             curr_len = get_len_of_current_expandable(&read_line[i + 1], env_list, exit_code);
             if (curr_len == 0)
@@ -78,7 +94,7 @@ char    *get_full_expandable_word_without_token_list(const char *read_line, t_en
     while (read_line[i])
     {
         set_quote_flag_and_count_single_quotes(&quote_flag, &single_quote_counter, read_line[i]);
-        if (is_dollar_sign(read_line[i]) && !is_end(read_line[i + 1]) && single_quote_counter % 2 == 0)//&& !is_quote(read_line[i + 1]))
+        if (eligible_for_expansion(read_line, i, single_quote_counter))
         {
             mock_expand = get_valid_expandable(read_line + i + 1);
             make_expansion(fully_expanded, mock_expand, env_list, exit_code);

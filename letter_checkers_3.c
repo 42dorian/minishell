a2bdae92d3b642bf92d6bc46bef46d0ddd4ca39b
Barna Koppany Guthy <bguthy@c2r1p7.42vienna.com>
2026-09-16 17:35:25 +0200

    NORMINETTED

--- letter_checkers_3.c
+++ letter_checkers_3.c
@@ -1,27 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   is_special_character.c                             :+:      :+:    :+:   */
+/*   letter_checkers_3.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bguthy <bguthy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 17:35:58 by bguhty            #+#    #+#             */
-/*   Updated: 2026/09/16 16:17:16 by bguthy           ###   ########.fr       */
+/*   Updated: 2026/09/16 17:34:23 by bguthy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	is_terminator(const char letter)
+int	is_special_character(const char letter1, const char letter2)
 {
-	if (letter == '\0')
+	if (is_heredoc_or_append(letter1, letter2))
+		return (1);
+	else if (is_redir_or_pipe(letter1))
+		return (1);
+	else
+		return (0);
+}
+
+int	is_white_space(const char letter)
+{
+	if ((letter <= CARRIAGE_RET && letter >= HORIZONTAL_TAB) || letter == SPACE)
 		return (1);
 	return (0);
 }
 
-int	is_astrisk(const char letter)
+int	is_delimeter(const char letter)
 {
-	if (letter == '*')
+	if (is_redir_or_pipe(letter))
 		return (1);
-	return (0);
+	else if (is_end(letter))
+		return (1);
+	else
+		return (0);
 }
--- stepping_in_input.c
+++ stepping_in_input.c
@@ -6,28 +6,11 @@
 /*   By: bguthy <bguthy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 20:34:44 by bguhty            #+#    #+#             */
-/*   Updated: 2026/09/16 16:34:36 by bguthy           ###   ########.fr       */
+/*   Updated: 2026/09/16 17:34:46 by bguthy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-int	is_white_space(const char letter)
-{
-	if ((letter <= CARRIAGE_RET && letter >= HORIZONTAL_TAB) || letter == SPACE)
-		return (1);
-	return (0);
-}
-
-int	is_delimeter(const char letter)
-{
-	if (is_redir_or_pipe(letter))
-		return (1);
-	else if (is_end(letter))
-		return (1);
-	else
-		return (0);
-}
 
 int	quote_in_word(const char *read_line, int *i, int *words)
 {
